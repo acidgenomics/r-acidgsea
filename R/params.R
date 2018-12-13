0141e7f2a7f38e79a1ac73c9dcdaac454a6908fd
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-13 13:43:22 -0500

    Update params documentation

--- R/params.R
+++ R/params.R
@@ -1,20 +1,33 @@
 #' Parameters
+#'
 #' @name params
 #' @keywords internal
-#' @param alpha `scalar numeric`. Alpha level.
-#' @param gmtFile `string`. MSigDB GMT file path.
-#' @param gmtFiles `character`. MSigDB GMT file paths.
-#' @param headerLevel `scalar integer`. Markdown header level.
-#' @param n `scalar integer`. Number of significant processes to include.
+#'
+#' @param alpha `scalar numeric`.
+#'   Alpha level.
+#' @param gmtFile `string`.
+#'   MSigDB GMT file path.
+#' @param gmtFiles `character`.
+#'   MSigDB GMT file paths.
+#' @param headerLevel `scalar integer`.
+#'   Markdown header level.
+#' @param n `scalar integer`.
+#'   Number of significant processes to include.
 #'   Bidirectional; will include `n` up- and down-regulated processes.
 #' @param object Object.
-#' @param pathways `character`. Pathways.
-#' @param results `data.frame`/`data.table`. Unmodified [fgsea::fgsea()] return.
-#' @param resultsList `list`. Results list.
-#' @param stats `numeric`. Gene stats. Test statistic is recommended by default,
-#'   but shrunken log2 fold changes are also acceptable. Don't input unshrunken
-#'   LFC values without standard error correction.
-#' @param statsList `list`. Gene stats list.
-#' @param statsList `list`. Gene stats list.
+#' @param pathways `character`.
+#'   Pathways.
+#' @param results `data.frame`/`data.table`.
+#'   Unmodified `fgsea::fgsea` return.
+#' @param resultsList `list`.
+#'   Results list.
+#' @param stats `numeric`.
+#'   Gene stats. Test statistic is recommended by default, but shrunken log2
+#'   fold changes are also acceptable. Don't input unshrunken LFC values without
+#'   standard error correction.
+#' @param statsList `list`.
+#'   Gene stats list.
+#' @param statsList `list`.
+#'   Gene stats list.
 #' @param ... Additional arguments.
 NULL
