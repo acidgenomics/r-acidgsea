e360c62c5abe1ddc47d3fd8978b490b068de793a
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-13 13:42:28 -0500

    Update assert checks and documentation

--- R/fgsea.R
+++ R/fgsea.R
@@ -1,7 +1,7 @@
 #' Fast Gene Set Enrichment Analysis
 #'
-#' Wrapper for [fgsea::fgsea()] that loads the pathways automatically from a
-#' GMT file, rather than requiring a separate call to [fgsea::gmtPathways()].
+#' Wrapper for `fgsea::fgsea` that loads the pathways automatically from a
+#' GMT file, rather than requiring a separate call to `fgsea::gmtPathways`.
 #'
 #' @name fgsea
 #' @inheritParams params
@@ -12,21 +12,14 @@ NULL
 #' @describeIn fgsea Returns a `data.frame`/`data.table` of results.
 #' @export
 fgsea <- function(stats, gmtFile, ...) {
-    assert_is_numeric(stats)
-    assert_all_are_existing_files(gmtFile)
+    assert(
+        is.numeric(stats),
+        isAFile(gmtFile)
+    )
     pathways <- gmtPathways(gmt.file = gmtFile)
-
     message(paste0("GMT file: ", basename(gmtFile)))
     message(paste("Testing against", length(pathways), "pathways."))
-
-    # Expecting warning about ranked tie, which isn't informative.
-    suppressWarnings(
-        fgsea::fgsea(
-            pathways = pathways,
-            stats = stats,
-            ...
-        )
-    )
+    fgsea::fgsea(pathways = pathways, stats = stats, ...)
 }
 
 
@@ -34,19 +27,19 @@ fgsea <- function(stats, gmtFile, ...) {
 #' @describeIn fgsea Parameterized variant. Returns a `list` of results.
 #' @export
 pfgsea <- function(gmtFiles, statsList, ...) {
-    assert_all_are_existing_files(gmtFiles)
-    assert_has_names(gmtFiles)
-    assert_is_list(statsList)
-    assert_has_names(statsList)
+    assert(
+        all(isFile(gmtFiles)),
+        hasNames(gmtFiles),
+        is.list(statsList),
+        hasNames(statsList)
+    )
     lapply(
         X = gmtFiles,
         FUN = function(gmtFile) {
-            assert_is_a_string(gmtFile)
-            assert_all_are_existing_files(gmtFile)
             lapply(
                 X = statsList,
                 FUN = function(stats) {
-                    assert_is_numeric(stats)
+                    assert(is.numeric(stats))
                     fgsea(stats = stats, gmtFile = gmtFile, ...)
                 }
             )
