e2a150d2f52b36271442eb6e0ae312591eae0ea0
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-13 13:42:38 -0500

    Simplify the NAMESPACE

--- R/package.R
+++ R/package.R
@@ -5,19 +5,14 @@
 #'
 #' @aliases NULL
 #'
-#' @importFrom assertive.base assert_are_identical
-#' @importFrom assertive.files assert_all_are_existing_files
-#' @importFrom assertive.properties assert_has_names
-#' @importFrom assertive.types assert_is_a_number assert_is_a_string
-#'   assert_is_character assert_is_data.frame assert_is_list assert_is_numeric
-#' @importFrom basejump assertIsAlpha assertIsHeaderLevel markdownHeader
-#'   theme_paperwhite
+#' @importFrom basejump as_tibble markdownHeader theme_paperwhite
 #' @importFrom dplyr arrange desc filter select
 #' @importFrom fgsea fgsea gmtPathways plotEnrichment plotGseaTable
 #' @importFrom ggplot2 labs
+#' @importFrom goalie assert containsAlpha containsHeaderLevel hasNames isAFile
+#'   isFile
 #' @importFrom knitr kable
 #' @importFrom magrittr %>%
 #' @importFrom rlang !! !!! sym syms
-#' @importFrom tibble as_tibble
 #' @importFrom utils head tail
 "_PACKAGE"
