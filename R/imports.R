09b4c8fb14d4c6eaea3bf9e59190d032fa25fa20
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-01 16:31:57 -0500

    Draft update to add `export()` method support

--- R/imports.R
+++ R/imports.R
@@ -1,15 +1,15 @@
 #' @importMethodsFrom DESeqAnalysis coerce
 #'
-#' @importFrom S4Vectors SimpleList metadata metadata<-
-#' @importFrom basejump Gene2Symbol as_tibble markdownHeader matchArgsToDoCall
-#'   theme_paperwhite
+#' @importFrom S4Vectors SimpleList lapply metadata metadata<-
+#' @importFrom basejump Gene2Symbol as_tibble export initDir markdownHeader
+#'   matchArgsToDoCall standardizeCall theme_paperwhite
 #' @importFrom bioverbs rankedList
 #' @importFrom dplyr arrange desc distinct filter group_by left_join select
 #'   summarize
 #' @importFrom fgsea fgsea gmtPathways plotEnrichment plotGseaTable
 #' @importFrom ggplot2 labs
 #' @importFrom goalie assert containsAlpha containsHeaderLevel hasNames isAFile
-#'   isCharacter isFile isInt isSubset validate
+#'   isCharacter isFile isInt isString isSubset validate
 #' @importFrom knitr kable
 #' @importFrom magrittr %>%
 #' @importFrom methods as is new setAs setGeneric setMethod validObject
