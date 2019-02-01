a5fb8414bd487b67c2f501ccd4bfaf3ad7a0d220
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-01 12:30:08 -0500

    Update NAMESPACE

---
+++ R/imports.R
@@ -0,0 +1,19 @@
+#' @importMethodsFrom DESeqAnalysis coerce
+#'
+#' @importFrom S4Vectors SimpleList
+#' @importFrom basejump Gene2Symbol as_tibble markdownHeader matchArgsToDoCall
+#'   theme_paperwhite
+#' @importFrom dplyr arrange desc distinct filter group_by left_join select
+#'   summarize
+#' @importFrom fgsea fgsea gmtPathways plotEnrichment plotGseaTable
+#' @importFrom ggplot2 labs
+#' @importFrom goalie assert containsAlpha containsHeaderLevel hasNames isAFile
+#'   isCharacter isFile isInt validate
+#' @importFrom knitr kable
+#' @importFrom magrittr %>%
+#' @importFrom methods as is setAs setGeneric setMethod validObject
+#' @importFrom rlang !! !!! := sym syms
+#' @importFrom stats na.omit
+#' @importFrom tibble deframe
+#' @importFrom utils head tail
+NULL
--- R/package.R
+++ R/package.R
@@ -5,22 +5,4 @@
 #'
 #' @aliases NULL
 #' @keywords internal
-#'
-#' @importMethodsFrom DESeqAnalysis coerce
-#'
-#' @importFrom basejump Gene2Symbol as_tibble markdownHeader matchArgsToDoCall
-#'   theme_paperwhite
-#' @importFrom dplyr arrange desc distinct filter group_by left_join select
-#'   summarize
-#' @importFrom fgsea fgsea gmtPathways plotEnrichment plotGseaTable
-#' @importFrom ggplot2 labs
-#' @importFrom goalie assert containsAlpha containsHeaderLevel hasNames isAFile
-#'   isCharacter isFile isInt
-#' @importFrom knitr kable
-#' @importFrom magrittr %>%
-#' @importFrom methods as is setGeneric setMethod validObject
-#' @importFrom rlang !! !!! := sym syms
-#' @importFrom stats na.omit
-#' @importFrom tibble deframe
-#' @importFrom utils head tail
 "_PACKAGE"
