bfe6831a457f7f837f213539461e04bbe2472d4f
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-04 10:32:05 -0500

    Switch `plotEnrichment()` and `plotGSEATable()` to S4 methods

---
+++ R/AllGenerics.R
@@ -0,0 +1,19 @@
+#' @rdname plotEnrichment
+#' @export
+setGeneric(
+    name = "plotEnrichment",
+    def = function(object, ...) {
+        standardGeneric("plotEnrichment")
+    }
+)
+
+
+
+#' @rdname plotGSEATable
+#' @export
+setGeneric(
+    name = "plotGSEATable",
+    def = function(object, ...) {
+        standardGeneric("plotGSEATable")
+    }
+)
