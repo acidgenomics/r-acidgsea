e07edcfe6b8a5bb9d190f2d8959fcfe1ee8ea57c
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-04 11:35:40 -0500

    Switch back to arranging by padj first then NES

--- R/filterResults-internal.R
+++ R/filterResults-internal.R
@@ -1,12 +1,13 @@
 .filterResults <- function(data, alpha) {
-    assert(
-        is(data, "data.table"),
-        isAlpha(alpha)
-    )
-    data %>%
+    assert(is(data, "data.table"), isAlpha(alpha))
+    data <- data %>%
         as_tibble() %>%
         filter(!!sym("padj") < !!alpha) %>%
-        arrange(desc(!!sym("NES")), !!sym("padj")) %>%
+        arrange(!!sym("padj"), desc(!!sym("NES"))) %>%
         # Prioritize the first columns.
-        select(!!!syms(c("pathway", "NES", "padj")), everything())
+        select(!!!syms(c("pathway", "padj", "NES")), everything())
+    if (!hasRows(data)) {
+        message(paste("No significant pathways at alpha < ", alpha, "."))
+    }
+    data
 }
