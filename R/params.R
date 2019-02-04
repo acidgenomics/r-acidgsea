70bd0d5c30b96aaff31f1e357fc9a91082ec5819
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-04 09:33:29 -0500

    Update documentation

--- R/params.R
+++ R/params.R
@@ -5,6 +5,8 @@
 #'
 #' @param alpha `numeric(1)`.
 #'   Alpha level.
+#' @param geneSet `character(1)`.
+#'   Gene set name. Can obtain using `names()` on `FGSEAList` object.
 #' @param gmtFile `character(1)`.
 #'   MSigDB GMT file path.
 #' @param gmtFiles `character`.
