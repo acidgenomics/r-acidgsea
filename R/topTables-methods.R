dfda82a8c446a2a015c4f1f3fd95e54ad20d4e66
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-04 09:25:37 -0500

    Convert `topTables()` to S4 method that works on FGSEAList object

---
+++ R/topTables-methods.R
@@ -0,0 +1,72 @@
+#' @name topTables
+#' @description Top tables of significantly enriched pathways.
+#' @details
+#' Supports looping across multiple DEG results, and adds a Markdown header for
+#' each contrast.
+#'
+#' @inherit bioverbs::topTables
+#' @inheritParams params
+#'
+#' @return Markdown output.
+NULL
+
+
+
+#' @importFrom bioverbs topTables
+#' @aliases NULL
+#' @export
+bioverbs::topTables
+
+
+
+topTables.FGSEAList <- function(
+    object,
+    geneSet,
+    alpha = 0.05,
+    n = 10L,
+    headerLevel = 2L
+) {
+    validObject(object)
+    assert(
+        isString(geneSet),
+        isSubset(geneSet, names(object)),
+        isAlpha(alpha),
+        isInt(n),
+        isHeaderLevel(headerLevel)
+    )
+    results <- object[[geneSet]]
+    invisible(mapply(
+        header = names(results),
+        data = results,
+        FUN = function(header, data) {
+            markdownHeader(text = header, level = headerLevel, asis = TRUE)
+            data <- data %>%
+                as_tibble() %>%
+                # Drop the nested list columns (e.g. leadingEdge, nMoreExtreme).
+                select_if(is.atomic) %>%
+                # Filter and arrange by desired significance.
+                filter(!!sym("padj") < !!alpha) %>%
+                arrange(desc(!!sym("NES")), !!sym("padj")) %>%
+                # Drop uninformative columns.
+                # Note: dplyr `-UQS` approach doesn't work for SE drops.
+                select(!!!syms(
+                    setdiff(colnames(.), c("ES", "nMoreExtreme", "pval"))
+                )) %>%
+                # Now we can subset to the desired number of top pathways.
+                head(n = n)
+            print(kable(data, digits = 3L))
+        },
+        SIMPLIFY = FALSE,
+        USE.NAMES = FALSE
+    ))
+}
+
+
+
+#' @rdname topTables
+#' @export
+setMethod(
+    f = "topTables",
+    signature = signature("FGSEAList"),
+    definition = topTables.FGSEAList
+)
