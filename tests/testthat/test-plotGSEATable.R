ecf46b21db00ac9dd2c616a55e6585280e01eed3
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-10 15:38:35 -0400

    Add code coverage

---
+++ tests/testthat/test-plotGSEATable.R
@@ -0,0 +1,16 @@
+context("plotGSEATable")
+
+test_that("FGSEAList", {
+    output <- capture.output(
+        plotGSEATable(gsea, geneSet = "h", n = 2L)
+    )
+    expect_identical(
+        output,
+        c(
+            "",
+            "",
+            "### dmso_r1881_vs_etoh",
+            ""
+        )
+    )
+})
