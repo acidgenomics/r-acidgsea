032b35a84382bef94801e9643332a6ec37f74859
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 09:44:49 -0400

    Set up docker checks

--- travis-docker.R
+++ travis-docker.R
@@ -1,3 +1,15 @@
-rcmdcheck::rcmdcheck(args = "--no-manual")
-BiocCheck::BiocCheck()
+# Copy example MSigDb files to `$HOME`.
+file.copy(
+    from = system.file(
+        "extdata", "msigdb",
+        package = "pfgsea",
+        mustWork = TRUE
+    ),
+    to = "~",
+    overwrite = FALSE,
+    recursive = TRUE
+)
+
+rcmdcheck::rcmdcheck(args = "--no-manual", error_on = "error")
+BiocCheck::BiocCheck(`quit-with-status` = TRUE)
 lintr::lint_package()
