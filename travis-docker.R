8e68e5474ed9fab6020792329ef5de869e95dd10
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 13:09:01 -0400

    Improve MSigDb example file handling

--- travis-docker.R
+++ travis-docker.R
@@ -1,17 +1,4 @@
-file.copy(
-    from = file.path("", "pfgsea", "inst", "extdata", "msigdb"),
-    to = "~",
-    overwrite = FALSE,
-    recursive = TRUE
-)
-stopifnot(file.exists(
-    file.path(
-        "~",
-        "msigdb",
-        "msigdb_v6.2_GMTs",
-        "h.all.v6.2.symbols.gmt"
-    )
-))
+source("msigdb.R")
 
 rcmdcheck::rcmdcheck(args = "--no-manual", error_on = "error")
 BiocCheck::BiocCheck(`quit-with-status` = TRUE)
