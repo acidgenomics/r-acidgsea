844f4c2c5c4f69af8bc3c76e41c87a543785f6de
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 13:04:56 -0400

    Copy MSigDB example file

--- travis-docker.R
+++ travis-docker.R
@@ -1,4 +1,19 @@
-source("copy-msigdb-files.R")
+file.copy(
+    from = file.path("", "pfgsea", "inst", "extdata", "msigdb"),
+    to = "~",
+    overwrite = FALSE,
+    recursive = TRUE
+)
+stopifnot(file.exists(
+    file.path(
+        "~",
+        "msigdb",
+        "msigdb_v6.2_GMTs",
+        "h.all.v6.2.symbols.gmt"
+    )
+))
+
 rcmdcheck::rcmdcheck(args = "--no-manual", error_on = "error")
 BiocCheck::BiocCheck(`quit-with-status` = TRUE)
+
 lintr::lint_package()
