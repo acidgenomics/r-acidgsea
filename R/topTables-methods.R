2b26103c43cf0c9af5322a6ad6a054a94cdf5bc4
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-12 11:39:16 -0400

    Pass lintr checks

--- R/topTables-methods.R
+++ R/topTables-methods.R
@@ -28,88 +28,91 @@ NULL
 
 
 
-topTables.FGSEAList <- function(
-    object,
-    collection,
-    n = 10L,
-    headerLevel = 3L
-) {
-    validObject(object)
-    alpha <- alphaThreshold(object)
-    assert(
-        isScalar(collection),
-        isAlpha(alpha),
-        isInt(n),
-        isHeaderLevel(headerLevel)
-    )
-    data <- object[[collection]]
-    invisible(mapply(
-        contrast = names(data),
-        data = data,
-        MoreArgs = list(
-            alpha = alpha,
-            n = n,
-            headerLevel = headerLevel
-        ),
-        FUN = function(contrast, data, alpha, n, headerLevel) {
-            markdownHeader(
-                text = contrast,
-                level = headerLevel,
-                asis = TRUE
-            )
+# Modified 2019-06-12.
+topTables.FGSEAList <-  # nolint
+    function(
+        object,
+        collection,
+        n = 10L,
+        headerLevel = 3L
+    ) {
+        validObject(object)
+        alpha <- alphaThreshold(object)
+        assert(
+            isScalar(collection),
+            isAlpha(alpha),
+            isInt(n),
+            isHeaderLevel(headerLevel)
+        )
+        data <- object[[collection]]
+        invisible(mapply(
+            contrast = names(data),
+            data = data,
+            MoreArgs = list(
+                alpha = alpha,
+                n = n,
+                headerLevel = headerLevel
+            ),
+            FUN = function(contrast, data, alpha, n, headerLevel) {
+                markdownHeader(
+                    text = contrast,
+                    level = headerLevel,
+                    asis = TRUE
+                )
 
-            # Filter our results, and early return if nothing's significant.
-            data <- .filterResults(data, alpha = alpha)
-            if (!hasRows(data)) {
-                return(invisible())
-            }
+                # Filter our results, and early return if nothing's significant.
+                data <- .filterResults(data, alpha = alpha)
+                if (!hasRows(data)) {
+                    return(invisible())
+                }
 
-            # Sanitize and minimize the results before printing.
-            data <- data %>%
-                # Drop the nested list columns (e.g. leadingEdge, nMoreExtreme).
-                select_if(is.atomic) %>%
-                # Drop additional uninformative columns.
-                # Note: dplyr `-UQS` approach doesn't work for SE drops.
-                select(!!!syms(
-                    setdiff(colnames(.), c("ES", "nMoreExtreme", "pval"))
-                ))
+                # Sanitize and minimize the results before printing.
+                data <- data %>%
+                    # Drop the nested list columns
+                    # (e.g. leadingEdge, nMoreExtreme).
+                    select_if(is.atomic) %>%
+                    # Drop additional uninformative columns.
+                    # Note: dplyr `-UQS` approach doesn't work for SE drops.
+                    select(!!!syms(
+                        setdiff(colnames(.), c("ES", "nMoreExtreme", "pval"))
+                    ))
 
-            # Generate our subset tibbles to print.
-            up <- data %>%
-                filter(!!sym("NES") > 0L) %>%
-                arrange(!!sym("padj"), desc(!!sym("NES"))) %>%
-                head(n = n)
-            down <- data %>%
-                filter(!!sym("NES") < 0L) %>%
-                arrange(!!sym("padj"), !!sym("NES")) %>%
-                head(n = n)
+                # Generate our subset tibbles to print.
+                up <- data %>%
+                    filter(!!sym("NES") > 0L) %>%
+                    arrange(!!sym("padj"), desc(!!sym("NES"))) %>%
+                    head(n = n)
+                down <- data %>%
+                    filter(!!sym("NES") < 0L) %>%
+                    arrange(!!sym("padj"), !!sym("NES")) %>%
+                    head(n = n)
 
-            markdownHeader(
-                text = "Upregulated",
-                level = headerLevel + 1L,
-                asis = TRUE
-            )
-            if (hasRows(up)) {
-                print(kable(up, digits = 3L))
-            } else {
-                message("No upregulated sets.")
-            }
+                markdownHeader(
+                    text = "Upregulated",
+                    level = headerLevel + 1L,
+                    asis = TRUE
+                )
+                if (hasRows(up)) {
+                    print(kable(up, digits = 3L))
+                } else {
+                    message("No upregulated sets.")
+                }
 
-            markdownHeader(
-                text = "Downregulated",
-                level = headerLevel + 1L,
-                asis = TRUE
-            )
-            if (hasRows(down)) {
-                print(kable(down, digits = 3L))
-            } else {
-                message("No downregulated sets.")
-            }
-        },
-        SIMPLIFY = FALSE,
-        USE.NAMES = FALSE
-    ))
-}
+                markdownHeader(
+                    text = "Downregulated",
+                    level = headerLevel + 1L,
+                    asis = TRUE
+                )
+                if (hasRows(down)) {
+                    print(kable(down, digits = 3L))
+                } else {
+                    message("No downregulated sets.")
+                }
+            },
+            SIMPLIFY = FALSE,
+            USE.NAMES = FALSE
+        ))
+    }
 
 
 
