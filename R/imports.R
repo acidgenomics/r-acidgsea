eb4c2ef783f3cd7031165894a58eb117b2f85eb7
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-26 13:35:33 -0400

    NAMESPACE fixes

--- R/imports.R
+++ R/imports.R
@@ -1,20 +1,22 @@
 #' @importClassesFrom DESeqAnalysis DESeqAnalysis
 #' @importMethodsFrom DESeqAnalysis coerce
 #'
-#' @importFrom S4Vectors SimpleList lapply metadata metadata<-
+#' @importFrom DESeqAnalysis DESeqAnalysis
+#' @importFrom S4Vectors SimpleList lapply mcols mcols<- metadata metadata<-
+#' @importFrom SummarizedExperiment rowRanges rowRanges<-
 #' @importFrom UpSetR fromList
 #' @importFrom acidplots acid_theme_light upset
 #' @importFrom basejump Gene2Symbol alphaThreshold as_tibble export initDir
-#'   makeNames markdownHeader matchArgsToDoCall showHeader showSlotInfo
-#'   standardizeCall
+#'   makeNames markdownHeader matchArgsToDoCall matchHumanOrthologs showHeader
+#'   showSlotInfo standardizeCall
 #' @importFrom dplyr arrange desc distinct everything filter group_by left_join
 #'   pull select select_if summarise
 #' @importFrom fgsea fgsea gmtPathways plotEnrichment plotGseaTable
 #' @importFrom ggplot2 labs
 #' @importFrom goalie areDisjointSets areSameLength assert bapply hasColnames
-#'   hasNames hasLength hasRownames hasRows isAFile isAll isAlpha isCharacter
-#'   isFile isFlag isHeaderLevel isInt isScalar isScalarInteger isString
-#'   isSubset validate
+#'   hasNames hasLength hasRownames hasRows isAFile isAll isAlpha isAny
+#'   isCharacter isFile isFlag isHeaderLevel isInt isScalar isScalarInteger
+#'   isString isSubset validate
 #' @importFrom knitr kable
 #' @importFrom magrittr %>%
 #' @importFrom methods as is new setAs setGeneric setMethod show slot slot<-
