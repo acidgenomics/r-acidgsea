caeffc2346a6847ca0445a77e8b88b73e6c83527
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-28 08:36:50 -0400

    Simplify internal code, now that we're requiring R 3.6

--- R/convertToHuman-methods.R
+++ R/convertToHuman-methods.R
@@ -1,9 +1,9 @@
 #' Convert data set to use human orthologs
 #'
+#' @name convertToHuman
 #' @note Currently requires at least Bioconductor 3.9, due to a Genomic Ranges
 #'   subsetting issue.
-#'
-#' @name convertToHuman
+#' @note Updated 2019-08-28.
 #'
 #' @inheritParams params
 #' @param map `DataFrame`, or `NULL`.
@@ -16,36 +16,23 @@
 #'   Features (i.e. rownames) will be remapped to human genes.
 #'
 #' @examples
-#' if (BiocManager::version() >= "3.9") {
-#'     data(deseq, package = "DESeqAnalysis")
-#'     convertToHuman(deseq)
-#' }
+#' data(deseq, package = "DESeqAnalysis")
+#' convertToHuman(deseq)
 NULL
 
 
 
-## Updated 2019-08-13.
+## Updated 2019-08-28.
 `convertToHuman,DESeqAnalysis` <-  # nolint
     function(object, map = NULL) {
-        ## Check for unsupported version of Bioconductor.
-        assert(requireNamespace("BiocManager", quietly = TRUE))
-        if (BiocManager::version() < "3.9") {
-            stop(
-                "This function is currently supported on Bioconductor 3.9+ ",
-                "due to a subsetting issue that affects Genomic Ranges.",
-            )
-        }
-
         validObject(object)
         assert(isAny(map, c("DataFrame", "NULL")))
-
         ## Break out the slots of the object.
         data <- as(object, "DESeqDataSet")
         transform <- as(object, "DESeqTransform")
         results <- slot(object, "results")
         lfcShrink <- slot(object, "lfcShrink")
         genes <- rownames(data)
-
         ## Get the organism and Ensembl release from DESeqDataSet.
         rrMeta <- metadata(rowRanges(data))
         ## This step shouldn't get hit but it's useful to keep as a check.
@@ -67,13 +54,11 @@ NULL
             "%s (Ensembl %d) detected.",
             organism, ensemblRelease
         ))
-
         ## Early return on Homo sapiens.
-        if (organism == "Homo sapiens") {
+        if (identical(organism, "Homo sapiens")) {
             message("Returning unmodified.")
             return(object)
         }
-
         ## Now we're ready to match the human orthologs.
         ## Note that this step can time out, so we're allowing map passthrough.
         if (is.null(map)) {
@@ -90,7 +75,6 @@ NULL
                 )
             )
         }
-
         map <- map[genes, , drop = FALSE]
         assert(identical(rownames(data), rownames(map)))
         keep <- !is.na(map[["hgncID"]])
@@ -100,7 +84,6 @@ NULL
             sum(keep, na.rm = TRUE),
             nrow(map)
         ))
-
         ## Perform subset operations.
         filterList <- function(x) {
             lapply(
@@ -110,19 +93,11 @@ NULL
                 }
             )
         }
-
         map <- map[keep, , drop = FALSE]
-        ## This operation is failing on BioC 3.8, 3.7 due to a GRanges-related
-        ## subsetting issue. We can't subset the rowRanges as expected here.
-        ## Error: subscript is a NSBS object that is incompatible with the
-        ## current subsetting operation
-        ## https://support.bioconductor.org/p/100097/
-        ## https://support.bioconductor.org/p/74459/
         data <- data[keep, , drop = FALSE]
         transform <- transform[keep, , drop = FALSE]
         results <- filterList(results)
         lfcShrink <- filterList(lfcShrink)
-
         ## Remap the human ortholog gene identifiers onto the row names.
         genes <- map[["hgncID"]]
         assignRownames <- function(x, value) {
@@ -136,25 +111,21 @@ NULL
         rownames(transform) <- genes
         results <- assignRownames(results, genes)
         lfcShrink <- assignRownames(results, genes)
-
         ## Update gene-to-symbol mappings defined in rowRanges.
         mcols(rowRanges(data))[["geneID"]] <- map[["hgncID"]]
         mcols(rowRanges(data))[["geneName"]] <- map[["hgncName"]]
         mcols(rowRanges(transform))[["geneID"]] <- map[["hgncID"]]
         mcols(rowRanges(transform))[["geneName"]] <- map[["hgncName"]]
-
         out <- DESeqAnalysis(
             data = data,
             transform = transform,
             results = results,
             lfcShrink = lfcShrink
         )
-
         ## Ensure we're keeping any user-defined metadata.
         metadata <- metadata(object)
         metadata[["convertToHuman"]] <- TRUE
         metadata(out) <- metadata
-
         out
     }
 
