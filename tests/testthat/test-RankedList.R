ef48272ed0c814654aeef7e770aba8b4bc2951e9
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-28 11:52:16 -0400

    Improve code coverage

--- tests/testthat/test-RankedList.R
+++ tests/testthat/test-RankedList.R
@@ -1,12 +1,10 @@
 context("RankedList")
 
-value <- methodFormals(
+value <- eval(methodFormals(
     f = "RankedList",
     signature = "DESeqAnalysis",
     package = "pfgsea"
-) %>%
-    .[["value"]] %>%
-    eval()
+)[["value"]])
 
 with_parameters_test_that(
     "DESeqAnalysis", {
@@ -21,6 +19,17 @@ with_parameters_test_that(
 )
 
 rm(value)
+
+test_that("Average values for duplicate gene symbols", {
+    x <- deseq
+    rowData <- rowData(x@data)
+    rowData[["geneName"]][2L] <- rowData[["geneName"]][1L]
+    rowData[["geneName"]][4L] <- rowData[["geneName"]][3L]
+    rowData(x@data) <- rowData
+    y <- RankedList(x)
+    expect_s4_class(y, "RankedList")
+    expect_true(nrow(x@data) - length(y[[1]]) == 2L)
+})
 
 test_that("FGSEAList", {
     object <- RankedList(gsea)
