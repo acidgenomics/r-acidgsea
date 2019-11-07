8fefd77e5f012e3e3b2e08661ba676b59c1aedc4
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-07 10:16:02 -0500

    Provide update support for legacy objects that don't contain alpha level

--- R/updateObject-methods.R
+++ R/updateObject-methods.R
@@ -4,6 +4,10 @@
 #' @note Updated 2019-11-07.
 #'
 #' @inheritParams acidroxygen::params
+#' @param alpha `number(1)`.
+#'   Alpha level used in [pfgsea()] call.
+#'   Note that this is not necessarily the alpha level used to generate
+#'   `DESeqResults` object.
 #'
 #' @return Modified object.
 #'
@@ -24,7 +28,18 @@ NULL
 
 
 `updateObject,FGSEAList` <-  # nolint
-    function(object) {
+    function(object, alpha) {
+        ## Slot alpha if undefined.
+        if (!isSubset("alpha", names(metadata(object)))) {
+            message("Object does not contain alpha used to perform GSEA.")
+            if (missing(alpha)) {
+                alpha <- 0.05
+            }
+            assert(isAlpha(alpha))
+            message("Assigning alpha of ", alpha, " into 'metadata()'.")
+            metadata(object)[["alpha"]] <- alpha
+        }
+        validObject(object)
         object
     }
 
