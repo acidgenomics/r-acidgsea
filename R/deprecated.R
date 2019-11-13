28d81ba683f0ca793f436abf602b4fdbd408a939
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-13 11:43:16 -0500

    Draft update, migrating to roxygen2 7.0

--- R/deprecated.R
+++ R/deprecated.R
@@ -3,12 +3,12 @@
 
 
 #' @name defunct
-#' @inherit basejump::defunct
+#' @inherit basejump::defunct description return title
 #' @keywords internal
 NULL
 
 #' @name deprecated
-#' @inherit basejump::deprecated
+#' @inherit basejump::deprecated description return title
 #' @keywords internal
 NULL
 
