4ea5f517e91e9e8bd3a5732af22afbec34a4c7f8
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-18 15:36:29 -0500

    Consolidate shared internal functions

--- R/filterResults-internal.R
+++
@@ -1,8 +0,0 @@
-## Updated 2019-08-28.
-.filterResults <- function(data, alpha) {
-    assert(isAlpha(alpha))
-    data <- as(data, "DataFrame")
-    data <- data[data[["padj"]] < alpha, , drop = FALSE]
-    data <- data[order(data[["padj"]], -data[["NES"]]), , drop = FALSE]
-    data
-}
