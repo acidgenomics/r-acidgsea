f67f8235e428287c6f8f4433618760f0560056a9
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-17 07:26:34 -0400

    Update code coverage

--- tests/testthat/test-combine.R
+++ tests/testthat/test-combine.R
@@ -7,8 +7,8 @@ test_that("FGSEAList", {
         object = combine(x, y),
         regexp = "contrastNames"
     )
-    contrastNames(x) <- "x"
-    contrastNames(y) <- "y"
+    contrastNames(x) <- paste0("x_", contrastNames(x))
+    contrastNames(y) <- paste0("y_", contrastNames(y))
     c <- combine(x, y)
     expect_s4_class(c, "FGSEAList")
 })
--- tests/testthat/test-convertToHuman.R
+++ tests/testthat/test-convertToHuman.R
@@ -1,12 +1,21 @@
 context("convertToHuman")
 
-test_that("Already human", {
+test_that("Human (already)", {
     object <- convertToHuman(deseq)
     expect_identical(object, deseq)
 })
 
 test_that("Mouse", {
     mm <- readRDS(file.path("cache", "mm_deseq.rds"))
+    expect_s4_class(mm, "DESeqAnalysis")
+    expect_identical(
+        object = dim(mm@data),
+        expected = c(500L, 12L)
+    )
     hs <- convertToHuman(mm)
     expect_s4_class(hs, "DESeqAnalysis")
+    expect_identical(
+        object = dim(hs@data),
+        expected = c(484L, 12L)
+    )
 })
