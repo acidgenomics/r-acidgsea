70fc658421b1c703ecea32337baaf65a5c466a83
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-18 16:52:03 -0400

    Draft update to unit tests

--- tests/testthat/test-enrichedGeneSets.R
+++ tests/testthat/test-enrichedGeneSets.R
@@ -1,21 +1,30 @@
-context("enrichedGeneSets")
+context("enrichedGeneSets : FGSEAList")
 
-test_that("FGSEAList", {
-    object <- enrichedGeneSets(fgsea, collection = "h", flatten = TRUE)
-    expect_type(object, "list")
-    expect_identical(
-        object = names(object),
-        expected = c(
-            "condition_B_vs_A_up",
-            "condition_B_vs_A_down",
-            "treatment_D_vs_C_up",
-            "treatment_D_vs_C_down"
-        )
+args <- list(
+    object = fgsea,
+    collection = "h",
+    alpha = 0.7
+)
+
+test_that("Upregulated gene sets", {
+    object <- do.call(
+        what = enrichedGeneSets,
+        args = c(args, direction = "up")
     )
 })
 
-test_that("Disable flatten mode", {
-    object <- enrichedGeneSets(fgsea, collection = "h", flatten = FALSE)
+test_that("Downregulated gene sets", {
+    object <- do.call(
+        what = enrichedGeneSets,
+        args = c(args, direction = "down")
+    )
+})
+
+test_that("Enriched in both directions", {
+    object <- do.call(
+        what = enrichedGeneSets,
+        args = c(args, direction = "both")
+    )
     expect_type(object, "list")
     expect_identical(
         object = names(object),
@@ -23,5 +32,13 @@ test_that("Disable flatten mode", {
             "condition_B_vs_A",
             "treatment_D_vs_C"
         )
+    )
+    expect_identical(
+        object = length(object[["condition_B_vs_A"]]),
+        expected = 9L
+    )
+    expect_identical(
+        object = length(object[["treatment_D_vs_C"]]),
+        expected = 21L
     )
 })
