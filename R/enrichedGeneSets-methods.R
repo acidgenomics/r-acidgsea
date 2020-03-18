8bd1c292c39c8431bb88b3bebeba851ab96aa009
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-18 08:16:11 -0400

    Add nesThreshold and direction support

--- R/enrichedGeneSets-methods.R
+++ R/enrichedGeneSets-methods.R
@@ -4,8 +4,9 @@
 #'
 #' @name enrichedGeneSets
 #' @inherit acidgenerics::enrichedGeneSets
-#' @note Updated 2019-08-28.
+#' @note Updated 2020-03-18.
 #'
+#' @inheritParams acidroxygen::params
 #' @inheritParams params
 #' @param flatten `logical(1)`.
 #'   Flatten nested "up"/"down" directional enrichment vector subsets.
@@ -16,12 +17,15 @@
 #' Named list formatted as:
 #'
 #' 1. Gene set collection (e.g. "h" from MSigDb).
-#' 2. Contrast (e.g. "dmso_r1881_vs_etoh").
+#' 2. Contrast (e.g. "condition_B_vs_A").
 #' 3. Direction (e.g. "down" or "up").
+#'
+#' @seealso
+#' - `DESeqAnalysis::deg`.
 #'
 #' @examples
 #' data(gsea)
-#' enrichedGeneSets(gsea, collection = "h")
+#' enrichedGeneSets(gsea, collection = "h", alpha = 0.6)
 NULL
 
 
@@ -36,23 +40,31 @@ NULL
 
 
 ## @seealso `DESeqAnalysis::plotDEGUpset()`, for looping inspiration.
-## Updated 2019-08-28.
+## Updated 2020-03-18.
 `enrichedGeneSets,FGSEAList` <-  # nolint
     function(
         object,
         collection,
         alpha = NULL,
+        nesThreshold = NULL,
+        direction = c("both", "up", "down"),
         flatten = TRUE
     ) {
         validObject(object)
         if (is.null(alpha)) {
             alpha <- alphaThreshold(object)
         }
+        if (is.null(nesThreshold)) {
+            nesThreshold <- 0L
+        }
         assert(
             isScalar(collection),
-            isFlag(flatten),
-            isAlpha(alpha)
+            isAlpha(alpha),
+            isNumber(nesThreshold),
+            isFlag(flatten)
         )
+        direction <- match.arg(direction)
+        nesThreshold <- abs(nesThreshold)
         collection <- object[[collection]]
         assert(
             is.list(collection),
@@ -61,21 +73,39 @@ NULL
         perContrast <- mapply(
             name = names(collection),
             data = collection,
-            FUN = function(name, data) {
+            MoreArgs = list(
+                alpha = alpha,
+                nesThreshold = nesThreshold,
+                direction = direction
+            ),
+            FUN = function(
+                name,
+                data,
+                alpha,
+                nesThreshold,
+                direction
+            ) {
+                out <- list()
                 data <- as(data, "DataFrame")
                 ## Subset significant enrichment.
                 data <- data[data[["padj"]] < alpha, , drop = FALSE]
                 ## Upregulated.
-                up <- data[data[["NES"]] > 0L, , drop = FALSE]
-                up <- up[order(up[["padj"]], -up[["NES"]]), , drop = FALSE]
-                up <- up[["pathway"]]
+                if (isSubset(direction, c("both", "up"))) {
+                    up <- data[data[["NES"]] > nesThreshold, , drop = FALSE]
+                    up <- up[order(up[["padj"]], -up[["NES"]]), , drop = FALSE]
+                    up <- up[["pathway"]]
+                    out[["up"]] <- up
+                }
                 ## Downregulated.
-                down <- data[data[["NES"]] < 0L, , drop = FALSE]
-                down <- down[
-                    order(down[["padj"]], down[["NES"]]), , drop = FALSE
-                    ]
-                down <- down[["pathway"]]
-                list(up = up, down = down)
+                if (isSubset(direction, c("both", "down"))) {
+                    down <- data[data[["NES"]] < nesThreshold, , drop = FALSE]
+                    down <- down[
+                        order(down[["padj"]], down[["NES"]]), , drop = FALSE
+                        ]
+                    down <- down[["pathway"]]
+                    out[["down"]] <- down
+                }
+                out
             },
             SIMPLIFY = FALSE,
             USE.NAMES = TRUE
