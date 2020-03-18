601f796e5c56882b8124e2681a8836a4e42f5e3a
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-18 14:41:07 -0400

    Simplify internal directionality handling, using `enrichedGeneSets` method

--- R/topTables-methods.R
+++ R/topTables-methods.R
@@ -29,75 +29,108 @@ NULL
 
 
 
-## Updated 2020-01-27.
+## Updated 2020-03-18.
 `topTables,FGSEAList` <-  # nolint
     function(
         object,
         collection,
+        alpha = NULL,
+        nesThreshold = NULL,
         n = 10L,
         headerLevel = 3L
     ) {
         validObject(object)
-        alpha <- alphaThreshold(object)
         assert(
             isScalar(collection),
-            isAlpha(alpha),
             isInt(n),
             isHeaderLevel(headerLevel)
         )
+        args <- list(
+            object = object,
+            collection = collection,
+            alpha = alpha,
+            nesThreshold = nesThreshold
+        )
+        ## Upregulated gene sets.
+        suppressMessages(
+            up <- do.call(
+                what = enrichedGeneSets,
+                args = c(args, direction = "up")
+            )
+        )
+        ## Downregulated gene sets.
+        suppressMessages(
+            down <- do.call(
+                what = enrichedGeneSets,
+                args = c(args, direction = "down")
+            )
+        )
+        assert(
+            is.list(up),
+            is.list(down),
+            identical(names(up), names(down))
+        )
+        ## Loop across the contrasts.
         data <- object[[collection]]
+        assert(identical(names(data), names(up)))
         invisible(mapply(
-            contrast = names(data),
-            data = data,
+            name = names(data),     # contrast name
+            data = data,            # fgsta data.table output
+            up = up,                # upregulated pathways
+            down = down,            # downregulated pathways
             MoreArgs = list(
-                alpha = alpha,
-                n = n,
-                headerLevel = headerLevel
+                headerLevel = headerLevel,
+                n = n
             ),
-            FUN = function(contrast, data, alpha, n, headerLevel) {
+            FUN = function(
+                name,
+                data,
+                up,
+                down,
+                n,
+                headerLevel
+            ) {
+                idCol <- "pathway"
+                dropCols <- c("ES", "nMoreExtreme", "pval")
                 markdownHeader(
-                    text = contrast,
+                    text = name,
                     level = headerLevel,
                     asis = TRUE
                 )
-                ## Filter our results, and early return on no sig features.
-                data <- .filterResults(data, alpha = alpha)
-                if (!hasRows(data)) {
-                    return(invisible())  # nocov
-                }
+                data <- as(data, "DataFrame")
                 ## Sanitize and minimize the results before printing.
                 ## Drop the nested list columns (e.g. leadingEdge).
                 data <- selectIf(data, is.atomic)
                 ## Drop additional uninformative columns.
-                keep <- setdiff(colnames(data), c("ES", "nMoreExtreme", "pval"))
+                keep <- setdiff(colnames(data), dropCols)
                 data <- data[, keep, drop = FALSE]
+                ## Upregulated gene sets.
                 markdownHeader(
                     text = "Upregulated",
                     level = headerLevel + 1L,
                     asis = TRUE
                 )
-                up <- data[data[["NES"]] > 0L, , drop = FALSE]
-                up <- up[order(up[["padj"]], -up[["NES"]]), , drop = FALSE]
+                idx <- match(x = up, table = data[[idCol]])
+                up <- data[idx, , drop = FALSE]
                 up <- head(up, n = n)
                 if (hasRows(up)) {
                     print(kable(as.data.frame(up), digits = 3L))
                 } else {
-                    cli_alert_info("No upregulated sets.")  # nocov
+                    cli_alert_info("No upregulated gene sets.")  # nocov
                 }
+                ## Downregulated gene sets.
                 markdownHeader(
                     text = "Downregulated",
                     level = headerLevel + 1L,
                     asis = TRUE
                 )
-                down <- data[data[["NES"]] < 0L, , drop = FALSE]
-                down <- down[
-                    order(down[["padj"]], down[["NES"]]), , drop = FALSE
-                    ]
+                idx <- match(x = down, table = data[[idCol]])
+                down <- data[idx, , drop = FALSE]
                 down <- head(down, n = n)
                 if (hasRows(down)) {
                     print(kable(as.data.frame(down), digits = 3L))
                 } else {
-                    cli_alert_info("No downregulated sets.")  # nocov
+                    cli_alert_info("No downregulated gene sets.")  # nocov
                 }
             },
             SIMPLIFY = FALSE,
