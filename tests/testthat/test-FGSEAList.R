7d4d095aec6f4b269cf7b5435889da8fc268316c
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-20 20:20:52 -0400

    Update unit test

--- tests/testthat/test-FGSEAList.R
+++ tests/testthat/test-FGSEAList.R
@@ -5,7 +5,7 @@ skip_if_not(dir.exists(file.path("~", "msigdb")))
 rankedList <- metadata(fgsea)[["rankedList"]]
 gmtFiles <- metadata(fgsea)[["gmtFiles"]]
 
-test_that("FGSEAList", {
-    x <- FGSEAList(rankedList = rankedList, gmtFiles = gmtFiles)
+test_that("RankedList", {
+    x <- FGSEAList(object = rankedList, gmtFiles = gmtFiles)
     expect_s4_class(x, "FGSEAList")
 })
