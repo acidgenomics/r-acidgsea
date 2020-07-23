5d8e8787031d12f4263881068e8816fd73424538
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-23 17:47:26 -0400

    Improve plotGeneSet to support custom colors

--- R/plotGeneSet-methods.R
+++ R/plotGeneSet-methods.R
@@ -1,18 +1,22 @@
-## FIXME Rework internal code using fgsea::plotEnrichment as a guide.
-## That code currently hard-codes theme_bw.
-
-
-
 #' Plot gene set enrichment
 #'
 #' @name plotGeneSet
-#' @note Updated 2020-07-22.
+#' @note Updated 2020-07-23.
 #'
 #' @inheritParams acidroxygen::params
 #' @inheritParams params
+#' @param colors `character(5)`.
+#'   Named character color vector indicating:
+#'
+#'   - Line color.
+#'   - Minimum (negative) threshold line color.
+#'   - Maximum (positive) threhsold color.
+#'   - Y intercept at origin color.
+#'   - Ticks color.
+#'
 #' @param ... Additional arguments.
 #'
-#' @seealso [fgsea::plotEnrichment()].
+#' @seealso Modified version of [fgsea::plotEnrichment()].
 #'
 #' @examples
 #' ## This requires MSigDB to be installed at `${HOME}`.
@@ -29,14 +33,28 @@ NULL
 
 
 
+## Updated 2020-07-23.
 `plotGeneSet,FGSEAList` <-
     function(
         object,
         collection,
         contrast,
-        set
+        set,
+        colors = c(
+            line = "black",
+            min = acidplots::purpleOrange(n = 2L)[[1L]],
+            max = acidplots::purpleOrange(n = 2L)[[2L]],
+            ticks = "black",
+            yintercept = "black"
+        )
     ) {
         validObject(object)
+        assert(
+            isCharacter(colors),
+            areSetEqual(names(colors), names(eval(formals()[["colors"]])))
+        )
+        ## GSEA weight parameter. Refer to `fgsea::calcGseaStat` for details.
+        gseaParam <- 1L
         assert(
             isString(collection),
             isSubset(collection, names(object))
@@ -59,12 +77,76 @@ NULL
         )
         pathway <- pathways[[set]]
         stats <- rankedList[[contrast]]
-        p <- fgsea::plotEnrichment(pathway = pathway, stats = stats)
-        assert(is(p, "ggplot"))
-        p <- p + labs(
-            title = set,
-            subtitle = paste(collection, contrast, sep = " : ")
+        rnk <- rank(-stats)
+        ord <- order(rnk)
+        statsAdj <- stats[ord]
+        statsAdj <- sign(statsAdj) * (abs(statsAdj) ^ gseaParam)
+        statsAdj <- statsAdj / max(abs(statsAdj))
+        pathway <- unname(as.vector(na.omit(match(pathway, names(statsAdj)))))
+        pathway <- sort(pathway)
+        gseaRes <- calcGseaStat(
+            stats = statsAdj,
+            selectedStats = pathway,
+            gseaParam = 1L,
+            returnAllExtremes = TRUE,
+            returnLeadingEdge = FALSE,
+            scoreType = "std"
         )
+        bottoms <- gseaRes[["bottoms"]]
+        tops <- gseaRes[["tops"]]
+        n <- length(statsAdj)
+        xs <- as.vector(rbind(pathway - 1L, pathway))
+        ys <- as.vector(rbind(bottoms, tops))
+        toPlot <- data.frame(x = c(0L, xs, n + 1L), y = c(0L, ys, 0L))
+        diff <- (max(tops) - min(bottoms)) / 8L
+        x <- NULL
+        y <- NULL
+        p <- ggplot(
+            data = toPlot,
+            mapping = aes(x = x, y = y)
+        ) +
+            geom_hline(
+                yintercept = 0,
+                color = colors[["yintercept"]],
+                size = 0.5
+            ) +
+            geom_segment(
+                data = data.frame(x = pathway),
+                mapping = aes(
+                    x = x,
+                    y = -diff / 2L,
+                    xend = x,
+                    yend = diff / 2L
+                ),
+                color = colors[["ticks"]],
+                size = 0.1
+            ) +
+            geom_line(
+                color = colors[["line"]],
+                size = 1L
+            ) +
+            geom_hline(
+                yintercept = max(tops),
+                color = colors[["max"]],
+                linetype = "longdash",
+                size = 0.5
+            ) +
+            geom_hline(
+                yintercept = min(bottoms),
+                color = colors[["min"]],
+                linetype = "longdash",
+                size = 0.5
+            ) +
+            theme(
+                panel.border = element_blank(),
+                panel.grid.minor = element_blank()
+            ) +
+            labs(
+                title = set,
+                subtitle = paste(collection, contrast, sep = " : "),
+                x = "rank",
+                y = "enrichment score"
+            )
         p
     }
 
