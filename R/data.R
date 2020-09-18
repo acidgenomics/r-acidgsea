261dcef1a1bf72bad75c521a52decd9526a29460
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-18 14:50:56 -0400

    Add deseq example object

--- R/data.R
+++ R/data.R
@@ -1,4 +1,16 @@
+#' DESeqAnalysis example data
+#'
+#' Used to generate example `FGSEAList` object.
+#'
+#' @examples
+#' data(deseq)
+#' print(deseq)
+"deseq"
+
+
+
 #' FGSEAList example data
+#'
 #' @examples
 #' data(fgsea)
 #' print(fgsea)
