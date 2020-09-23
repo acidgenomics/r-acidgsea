21d4577dba4e4dee266305e74dc2d69f1184832d
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-23 10:19:18 -0400

    Don't save example DESeqAnalysis object

--- R/data.R
+++ R/data.R
@@ -1,14 +1,3 @@
-#' DESeqAnalysis example data
-#'
-#' Used to generate example `FGSEAList` object.
-#'
-#' @examples
-#' data(deseq)
-#' print(deseq)
-"deseq"
-
-
-
 #' FGSEAList example data
 #'
 #' @examples
