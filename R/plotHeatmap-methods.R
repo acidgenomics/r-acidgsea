8ac99a9b37cbbc7b7fc016dd5903ea7b3a45f767
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-23 10:52:17 -0400

    Simplify DESeqAnalysis handling

--- R/plotHeatmap-methods.R
+++ R/plotHeatmap-methods.R
@@ -1,16 +1,15 @@
 #' @name plotHeatmap
 #' @inherit acidplots::plotHeatmap description return title
-#' @note Updated 2020-09-21.
+#' @note Updated 2020-09-23.
 #'
 #' @inheritParams acidroxygen::params
 #' @inheritParams params
 #' @param ... Additional arguments.
 #'
 #' @examples
-#' data(fgsea, deseq)
+#' data(fgsea)
 #' plotHeatmap(
 #'     object = fgsea,
-#'     DESeqAnalysis = deseq,
 #'     contrast = "condition_B_vs_A",
 #'     collection = "h",
 #'     set = "HALLMARK_P53_PATHWAY"
@@ -28,11 +27,10 @@ NULL
 
 
 
-## Updated 2020-09-21.
+## Updated 2020-09-23.
 `plotHeatmap,FGSEAList` <-  # nolint
     function(
         object,
-        DESeqAnalysis,  # nolint
         contrast,
         contrastSamples = TRUE,
         collection,
@@ -40,15 +38,8 @@ NULL
         leadingEdge = FALSE,
         ...
     ) {
-        deseq <- DESeqAnalysis
         validObject(object)
-        validObject(deseq)
         assert(
-            is(deseq, "DESeqAnalysis"),
-            identical(
-                x = names(object[[1L]]),
-                y = contrastNames(deseq)
-            ),
             isString(contrast),
             isFlag(contrastSamples),
             isString(collection),
@@ -71,6 +62,7 @@ NULL
             )
         }
         ## Plot the log counts from DESeqTransform object.
+        deseq <- `DESeqAnalysis,FGSEAList`(object)
         dt <- as(deseq, "DESeqTransform")
         dt <- .matchGeneSet(
             object = dt,
