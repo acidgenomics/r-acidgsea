a1cee8e2882bf265a5dbcda474b1eb79dec617a5
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-23 10:32:09 -0400

    Comment out assert checks

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -39,6 +39,7 @@ setValidity(
                     ## "sessionInfo"
                     "alpha",
                     "collections",
+                    ## > "deseq",  # 0.4.0
                     "geneSetFiles",
                     "rankedList",
                     "version"
@@ -50,6 +51,7 @@ setValidity(
                 x = names(object),
                 y = names(metadata(object)[["collections"]])
             ),
+            ## > is(metadata(object)[["deseq"]], "DESeqAnalysis"),  # 0.4.0
             isCharacter(metadata(object)[["geneSetFiles"]]),
             identical(
                 x = names(object),
