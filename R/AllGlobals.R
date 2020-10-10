b9449051252c0f9e9fcf4214a5cfbcc670f8b5a8
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-10 10:38:33 -0400

    Update globals

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,15 +1,27 @@
-globalVariables(".")
+#' RankedList values
+#'
+#' @note Updated 2020-10-10.
+#' @noRd
+.rankedListValue <- c("stat", "log2FoldChange", "padj")
 
 
 
+#' Package version
+#'
+#' @note Updated 2020-10-10.
+#' @noRd
 .version <- packageVersion(packageName())
 
-#' acidgsea test data URL
+
+
+#' AcidGSEA test data URL
+#'
 #' @keywords internal
 #' @export
+#'
 #' @examples
-#' acidgseaTestsURL
-acidgseaTestsURL <- paste0(
-    "https://tests.acidgenomics.com/acidgsea/",
+#' AcidGSEATestsURL
+AcidGSEATestsURL <- paste0(
+    "https://tests.acidgenomics.com/AcidGSEA/",
     "v", .version$major, ".", .version$minor  # nolint
 )
