be9c4879a5e192c01bcef922a74844b3f235445e
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-04 14:56:21 -0500

    Add `prepareGeneSetFiles`

---
+++ R/prepareGeneSetFiles.R
@@ -0,0 +1,51 @@
+#' Prepare gene set files
+#'
+#' @details
+#' For example, intended for MSigDb releases.
+#'
+#' @note Updated 2021-03-04.
+#' @export
+#'
+#' @param dir `character(1)`.
+#'   Directory name containing MSigDb release.
+#'
+#' @examples
+#' dir <- system.file(
+#'     "extdata",
+#'     "msigdb",
+#'     "7.0",
+#'     "msigdb_v7.0_GMTs",
+#'     package = "AcidGSEA",
+#'     mustWork = TRUE
+#' )
+#' files <- prepareGeneSetFiles(dir)
+#' print(files)
+prepareGeneSetFiles <- function(dir) {
+    dir <- realpath(dir)
+    pattern <- "*.all.*.symbols.gmt"
+    files <- sort(list.files(
+        path = dir,
+        pattern = pattern,
+        full.names = TRUE,
+        recursive = FALSE
+    ))
+    if (!hasLength(files)) {
+        stop(sprintf(
+            "Failed to locate GMT files matching '%s' pattern in '%s'.",
+            pattern, dir
+        ))
+    }
+    names <- vapply(
+        X = strsplit(
+            x = basename(files),
+            split = ".",
+            fixed = TRUE
+        ),
+        FUN = `[[`,
+        1L,
+        FUN.VALUE = character(1L),
+        USE.NAMES = FALSE
+    )
+    names(files) <- names
+    files
+}
