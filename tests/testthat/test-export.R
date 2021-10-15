115873334f87bc3fa244c14c5d32b7b96ce9a82e
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-15 14:30:53 -0400

    Improve export code coverage

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -2,11 +2,54 @@ context("export : FGSEAList")
 
 testdir <- file.path(tempdir(), "example")
 
-## FIXME Need to add support for this.
 test_that("New 'con' BiocIO approach, instead of deprecated 'dir'", {
+    unlink(testdir, recursive = TRUE)
+    object <- fgsea
+    out <- export(
+        object = object,
+        con = testdir
+    )
+    prefix <- realpath(testdir)
+    expect_identical(
+        object = out,
+        expected = list(
+            "condition_B_vs_A" = list(
+                "h" = realpath(file.path(
+                    prefix,
+                    "condition_B_vs_A",
+                    "h.csv"
+                ))
+            ),
+            "treatment_D_vs_C" = list(
+                "h" = realpath(file.path(
+                    prefix,
+                    "treatment_D_vs_C",
+                    "h.csv"
+                ))
+            )
+        )
+    )
+    expect_identical(
+        object = sort(list.files(prefix)),
+        expected = c(
+            "condition_B_vs_A",
+            "treatment_D_vs_C"
+        )
+    )
+    expect_true(all(file.exists(
+        file.path(
+            prefix,
+            c(
+                "condition_B_vs_A",
+                "treatment_D_vs_C"
+            ),
+            "h.csv"
+        )
+    )))
+    unlink(testdir, recursive = TRUE)
 })
 
-test_that("FGSEAList", {
+test_that("Deprecated : 'dir' argument, no 'name'", {
     unlink(testdir, recursive = TRUE)
     object <- fgsea
     out <- export(
@@ -51,33 +94,4 @@ test_that("FGSEAList", {
         )
     )))
     unlink(testdir, recursive = TRUE)
-})
-
-test_that("name argument", {
-    files <- export(
-        object = fgsea,
-        name = "XXX",
-        dir = ".",
-        geneSetResults = FALSE
-    )
-    expect_identical(
-        object = files,
-        expected = list(
-            "condition_B_vs_A" = list(
-                "h" = realpath(file.path(
-                    "XXX",
-                    "condition_B_vs_A",
-                    "h.csv"
-                ))
-            ),
-            "treatment_D_vs_C" = list(
-                "h" = realpath(file.path(
-                    "XXX",
-                    "treatment_D_vs_C",
-                    "h.csv"
-                ))
-            )
-        )
-    )
-    unlink("XXX", recursive = TRUE)
 })
