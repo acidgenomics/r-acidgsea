37cb4816e9c9958f172e9144550d5cfc252363bb
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-27 11:23:01 -0400

    Resave example object

--- data-raw/fgsea.R
+++ data-raw/fgsea.R
@@ -1,14 +1,14 @@
 suppressPackageStartupMessages({
+    library(devtools)
     library(usethis)
     library(goalie)
     library(basejump) # 0.14.17
     library(DESeq2) # 1.30.1
     library(DESeqAnalysis) # 0.4.0
 })
-
+load_all()
 ## Restrict to 3 MB.
 limit <- structure(3e6, class = "object_size")
-
 gr <- makeGRangesFromEnsembl(
     organism = "Homo sapiens",
     level = "genes",
@@ -18,30 +18,22 @@ gr <- makeGRangesFromEnsembl(
 gr <- as(gr, "GenomicRanges")
 gr <- gr[sort(names(gr))]
 gr <- head(gr, n = 1000L)
-gr <- droplevels(gr)
+gr <- droplevels2(gr)
 mcols(gr) <- mcols(gr)[c("geneId", "geneName")]
-object.size(gr)
-## 337624 bytes
-
 ## DESeqDataSet
 dds <- makeExampleDESeqDataSet(n = length(gr), m = 12L)
 dds$treatment <- as.factor(x = rep(c("C", "D"), each = 3L))
 design(dds) <- ~ condition + treatment + condition:treatment
 rowRanges(dds) <- gr
 dds <- DESeq(dds)
-dds$condition
+## > dds$condition
 ## [1] A A A A A A B B B B B B
 ## Levels: A B
-dds$treatment
+## > dds$treatment
 ## [1] C C C D D D C C C D D D
 ## Levels: C D
-object.size(dds)
-## 939848 bytes
-
 ## DESeqTransform
 dt <- varianceStabilizingTransformation(dds)
-
-## DESeqResults
 contrasts <- list(
     "condition_B_vs_A" = c(
         factor = "condition",
@@ -61,7 +53,6 @@ res <- mapply(
     SIMPLIFY = FALSE,
     USE.NAMES = TRUE
 )
-
 ## DESeqAnalysis
 deseq <- DESeqAnalysis(
     data = dds,
@@ -69,9 +60,6 @@ deseq <- DESeqAnalysis(
     results = res,
     lfcShrink = NULL
 )
-object.size(deseq)
-## 1965104 bytes
-
 ## Just using hallmark in minimal example.
 geneSetFiles <- prepareGeneSetFiles(
     dir = system.file(
@@ -81,10 +69,8 @@ geneSetFiles <- prepareGeneSetFiles(
         "msigdb_v7.0_GMTs",
         package = "AcidGSEA",
         mustWork = TRUE
-    ),
-    pattern = "*.all.*.symbols.gmt"
+    )
 )
-
 fgsea <- FGSEAList(
     object = deseq,
     geneSetFiles = geneSetFiles,
@@ -96,5 +82,4 @@ assert(
     hasRows(fgsea[[1L]][[1L]]),
     is(metadata(fgsea)[["deseq"]], "DESeqAnalysis")
 )
-
 use_data(fgsea, overwrite = TRUE, compress = "xz")
