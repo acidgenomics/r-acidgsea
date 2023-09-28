3df0c4e5f0f68dbfa0494d3eb3c7228c5175c8f7
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 14:13:41 -0400

    Improve testdata file caching

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -1,25 +1,9 @@
-if (!isTRUE(goalie::hasInternet())) {
-    warning("No Internet connection detected.")
-    return(invisible(NULL))
-}
-dir.create("cache", showWarnings = FALSE)
-files <- c(
-    "h.all.v7.0.symbols.gmt",
-    "mm_deseq.rds"
-)
-Map(
-    f = function(remoteDir, file, envir) {
-        destfile <- file.path("cache", file)
-        if (!file.exists(destfile)) {
-            utils::download.file(
-                url = paste(remoteDir, file, sep = "/"),
-                destfile = destfile
-            )
-        }
-    },
-    file = files,
-    MoreArgs = list(
-        "remoteDir" = AcidGseaTestsUrl,
-        "envir" = environment()
+lst <- AcidDevTools::cacheTestFiles(
+    pkg = .pkgName,
+    files = c(
+        "h.all.v7.0.symbols.gmt",
+        "mm_deseq.rds"
     )
 )
+cacheDir <- lst[["cacheDir"]]
+rm(lst)
