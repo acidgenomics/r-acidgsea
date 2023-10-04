d8b938163b9f2a2072bea00cea3b6fef635782b4
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-04 14:46:14 -0400

    Update example object

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,3 +1,14 @@
+## nolint start
+`rowData<-` <- SummarizedExperiment::`rowData<-`
+data <- utils::data
+isAnExistingUrl <- goalie::isAnExistingUrl
+methodFormals <- AcidBase::methodFormals
+realpath <- AcidBase::realpath
+rowData <- SummarizedExperiment::rowData
+tempdir2 <- AcidBase::tempdir2
+unlink2 <- AcidBase::unlink2
+## nolint end
+
 data(
     deseq,
     package = "DESeqAnalysis",
@@ -13,13 +24,3 @@ data(
     package = "AcidGSEA",
     envir = environment()
 )
-
-## nolint start
-`rowData<-` <- SummarizedExperiment::`rowData<-`
-isAnExistingUrl <- goalie::isAnExistingUrl
-methodFormals <- AcidBase::methodFormals
-realpath <- AcidBase::realpath
-rowData <- SummarizedExperiment::rowData
-tempdir2 <- AcidBase::tempdir2
-unlink2 <- AcidBase::unlink2
-## nolint end
